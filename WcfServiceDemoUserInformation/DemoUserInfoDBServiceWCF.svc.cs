37383fe52a720c02cb399aadff1572956a72e228
davi7_64 <dffarjam2060@gmail.com>
2018-06-15 22:55:47 +0430

    successFully work WCF service

---
+++ WcfServiceDemoUserInformation/DemoUserInfoDBServiceWCF.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceDemoUserInformation
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DemoUserInfoDBServiceWCF" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DemoUserInfoDBServiceWCF.svc or DemoUserInfoDBServiceWCF.svc.cs at the Solution Explorer and start debugging.
+    public class DemoUserInfoDBServiceWCF : IDemoUserInfoDBServiceWCF
+    {
+        DB_ModelDemo_DBSmartAspEntities dbDemo = new DB_ModelDemo_DBSmartAspEntities();
+        public UserInformation find(string password)
+        {
+            return dbDemo.UserInformations.Single(p => p.UserPassword == password);
+        }
+
+        public List<UserInformation> findAll()
+        {
+            return dbDemo.UserInformations.ToList();
+        }
+    }
+}
